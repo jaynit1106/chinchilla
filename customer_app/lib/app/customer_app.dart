440c3d790af00df1f6b827bc37a4f9d3d804b529
abhibhaw <asthana3110@gmail.com>
2021-09-08 16:03:14 +0530

    Adds Lato font configs, removes unwanted data

--- customer_app/lib/app/customer_app.dart
+++ customer_app/lib/app/customer_app.dart
@@ -1,50 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CustomerApp extends StatelessWidget {
-  void answerQuestion() {
-    print('Answer chosen');
-  }
+  const CustomerApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<String> questions = ["What's your name?", "Who are you?"];
     return Scaffold(
       appBar: AppBar(
-        title: Text('Customer App'),
-        primary: true,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: answerQuestion,
-          ),
-          IconButton(
-            icon: Icon(Icons.notification_add),
-            onPressed: answerQuestion,
-          ),
-          IconButton(
-            icon: Icon(Icons.notification_important),
-            onPressed: answerQuestion,
-          ),
-          IconButton(
-            icon: Icon(Icons.notifications_active),
-            onPressed: answerQuestion,
-          ),
-        ],
-      ),
-      body: Column(
-        children: questions
-            .map((question) => Column(
-                  children: [
-                    Text(question),
-                    ElevatedButton(
-                        onPressed: answerQuestion, child: Text('Answer 1')),
-                    ElevatedButton(
-                        onPressed: answerQuestion, child: Text('Answer 2')),
-                    ElevatedButton(
-                        onPressed: answerQuestion, child: Text('Answer 3')),
-                  ],
-                ))
-            .toList(),
+        title: Text('Milkton'),
       ),
     );
   }
