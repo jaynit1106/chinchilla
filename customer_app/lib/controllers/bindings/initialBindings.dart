1c283c3d0fd24825193422d69964a2699b82319b
abhibhaw <asthana3110@gmail.com>
2021-09-19 02:33:39 +0530

    feat: Adds Remote Config closes #1

--- customer_app/lib/controllers/bindings/initialBindings.dart
+++ customer_app/lib/controllers/bindings/initialBindings.dart
@@ -1,9 +1,11 @@
-import 'package:customer_app/controllers/authController.dart';
 import 'package:get/get.dart';
+import 'package:customer_app/controllers/authController.dart';
+import 'package:customer_app/services/remote_config.dart';
 
 class InitialBinding implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut(() => RemoteConfigService());
     Get.lazyPut(() => AuthController());
   }
 }
---
+++ customer_app/lib/services/remote_config.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+
+class RemoteConfigService {
+  late RemoteConfig _remoteConfig;
+  setupRemoteConfig() async {
+    try {
+      _remoteConfig = RemoteConfig.instance;
+      await _remoteConfig.ensureInitialized();
+      await _remoteConfig.fetchAndActivate();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  String get apiKey => _remoteConfig.getString('api_key');
+  String get prodUrl => _remoteConfig.getString('prod_url');
+}
