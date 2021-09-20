a3b006a41cec9e45929904a43a99bc96f673389c
abhibhaw <asthana3110@gmail.com>
2021-09-20 09:06:19 +0530

    feat: Add graphql service init

---
+++ customer_app/lib/services/graphql_services.dart
@@ -0,0 +1,17 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:customer_app/utils/constants/apiSecrets.dart';
+
+class GraphQLService {
+  late ValueNotifier<GraphQLClient> client;
+  void setupGraphQL() async {
+    await initHiveForFlutter();
+    final HttpLink httpLink = HttpLink(kGraphQLApiUrl);
+    client = ValueNotifier(
+      GraphQLClient(
+        link: httpLink,
+        cache: GraphQLCache(store: HiveStore()),
+      ),
+    );
+  }
+}
